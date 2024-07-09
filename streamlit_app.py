88df13367eb8894373cd898607131fe587c3fff0
Statistical analysis and modelling (SCMA-632) VCU <87145248+scma-632@users.noreply.github.com>
2024-07-09 18:04:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,4 @@ import streamlit as st
 
 st.title("Hello, Everyone this is SCMA 632 Class!")
 st.write("We are learning about the streamlit web framework")
+st.write("this is the second changes to the app")
